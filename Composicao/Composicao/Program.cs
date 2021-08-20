f714c5e038f990e495ae57b4779841168a1541a2
Filipe Ventura <filipe.ventura@icloud.com>
2021-08-20 14:19:22 -0300

    composicao 2.1

--- Composicao/Composicao/Program.cs
+++ Composicao/Composicao/Program.cs
@@ -37,9 +37,17 @@ namespace Composicao
 
                 HourContract contract = new HourContract(date, valuePerHour, hours);
                 worker.AddContract(contract);
-
             }
 
+            Console.WriteLine();
+            Console.Write("Enter monthand year to calculate  income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ":" + worker.Income(year, month));
 
 
         }
